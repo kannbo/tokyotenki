4479d66caf0af027f8feee9578327ec3f951388a
kannbo <113285773+kannbo@users.noreply.github.com>
2024-04-14 13:37:32 +0900

    Update web.py

--- web.py
+++ web.py
@@ -3,7 +3,7 @@ import subprocess
 subprocess.Popen(["python3","tenki.py"])
 
 @route('/')
-def index(name):
+def index():
     return "<title>こんちゃ</title><h1>OKです"
 
 run(host='0.0.0.0', port="10000")
